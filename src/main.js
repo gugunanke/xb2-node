74ccb04fe26dc680d1ffbef49c29308ef41e67af
lizhuoran <1581817299@qq.com>
2022-01-12 11:26:32 +0800

    框架学习1

--- src/main.js
+++ src/main.js
@@ -1,21 +1,40 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const data = {
-    'id': 1,
-    'title': '关山月',
-    'content': '明日出天山，苍茫云海间',
-  }
-  const jsonData = JSON.stringify(data);
-  response.writeHead(200, {
-    'Content-Type': 'application/json;charset=utf-8'
-  });
-  response.write(jsonData);
-  response.end();
+app.listen(port, () => {
+  console.log('🚀🚀服务已启动！');
 });
 
-server.listen(3000, () => {
-  console.log('🚀服务已启动！');
+app.get('/', (request, response) => {
+  response.send('你好!');
 
+});
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明日出天山，苍茫云海间'
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小'
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝'
+  }
+];
 
+app.get('/posts', (request, response) => {
+  response.send(data);
+
+});
+app.get('/posts/:postId', (request, response) => {
+  console.log(request.params);
+  const { postId } = request.params;
+  const posts = data.filter(item => item.id == postId);
+  response.send(posts[0]);
 });
